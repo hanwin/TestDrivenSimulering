8766d6c892ff25f1df838c82efc2b961e44dbb8e
Martin <martin_backstrom8@hotmail.com>
2016-12-01 13:26:46 +0100

    Unittest v1.0

--- TestdrivenUtveckling.ino
+++ TestdrivenUtveckling.ino
@@ -1,4 +1,4 @@
-#include <DHT.h>;
+#include "DHT.h"
 
 
 #define DHTPIN 12     
@@ -23,6 +23,66 @@ void setup()
   pinMode(LED_YELLOW, OUTPUT);
   pinMode(LED_RED, OUTPUT);
   pinMode(BUTTON, INPUT);
+  unitTest_temp();
+  unitTest_hum();
+}
+
+int temp_condition_test( int temp_value, int expected_value ){
+  return ( temp_value == expected_value );
+}
+void unitTest_temp(){
+  int fail[50];
+  int i = 0, j = 0, k = 0;
+
+  for( ;i < 50; i++){
+    if (temp_condition_test( print_temp(i), j ) == 0){
+      fail[k] = print_temp(i);
+      k++;
+    }   
+    switch(i){
+      case 15: j++;
+      break;  
+      case 19: j++;
+      break;
+      case 24: j++;  
+      break;
+    }
+  }
+
+  if (!k)
+    Serial.println ( "TEMP TEST SUCCESS");
+    
+  for(i = 0; i < k; i++) 
+    Serial.println(fail[i]);
+}
+
+int hum_condition_test( int hum_value, int expected_value ){
+  return ( hum_value == expected_value );
+}
+void unitTest_hum(){
+  int fail[50];
+  int i = 0, j = 0, k = 0;
+
+  for( ;i < 50; i++){
+    if (hum_condition_test( print_hum(i), j ) == 0){
+      fail[k] = print_hum(i);
+      k++;
+    }    
+    switch(i){
+      case 45: j++;
+      break;  
+      case 49: j++;
+      break;
+      case 54: j++;  
+      break;
+    }
+  }
+
+  if (!k)
+    Serial.println ( "HUM TEST SUCCESS");
+    
+  for(i = 0; i < k; i++) 
+    Serial.println(fail[i]);
 }
 
 void loop()
@@ -70,7 +130,7 @@ int print_temp(int t){
       if (t > 15 && t < 20){
       status = 1;
     }
-    else if (t >= 20 && temp < 25 ){
+    else if (t >= 20 && t < 25 ){
       status = 2;
       
     }
@@ -95,4 +155,3 @@ int print_hum(int h){
     }
     return status;
 }
-   
